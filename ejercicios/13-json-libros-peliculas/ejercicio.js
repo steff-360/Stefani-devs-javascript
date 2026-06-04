2ffc52a0e078986114622ff67b1bd6dc2761207b
steff-360 <steff.18random@gmail.com>
2026-06-04 07:43:12 -0600

    Resolucion ejercicio 13

--- ejercicios/13-json-libros-peliculas/ejercicio.js
+++ ejercicios/13-json-libros-peliculas/ejercicio.js
@@ -5,3 +5,35 @@ console.log("Ejercicio 13: JSON");
 
 // Escribe tu solucion aqui.
 
+console.log("Ejercicio 13: JSON");
+
+// Array de objetos
+const favoritos = [
+    {
+        tipo: "Libro",
+        titulo: "Cien años de soledad",
+        autor: "Gabriel García Márquez"
+    },
+    {
+        tipo: "Película",
+        titulo: "Avatar",
+        director: "James Cameron"
+    }
+];
+
+// Convertir a JSON
+const datosJSON = JSON.stringify(favoritos);
+
+console.log("JSON generado:");
+console.log(datosJSON);
+
+// Convertir JSON a objeto
+const datosObjeto = JSON.parse(datosJSON);
+
+console.log("Datos recuperados:");
+console.log(datosObjeto);
+
+// Reto extra
+if (datosObjeto.length > 0) {
+    console.log("Hay elementos registrados en favoritos.");
+}
